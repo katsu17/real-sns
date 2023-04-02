3cfb695a114a7baae888f371ca0fc6a577bdc237
tandai katsuaki <>
2023-04-02 21:24:57 +0900

    fix: api url to axios of actionCalls

--- Frontend/src/actionCalls.js
+++ Frontend/src/actionCalls.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 export const loginCall = async (user, dispatch) => {
+  const client = axios.create({
+    baseURL: process.env.BACKEND_ACCESS,
+  });
+
   dispatch({ type: "LOGIN_START" });
   try {
     // userにはemailとpasswordが入る
-    const response = await axios.post("auth/login", user);
+    const response = await client("auth/login", user);
     dispatch({ type: "LOGIN_SUCCESS", payload: response.data });
   } catch (err) {
     dispatch({ type: "LOGIN_ERROR", payload: err });
