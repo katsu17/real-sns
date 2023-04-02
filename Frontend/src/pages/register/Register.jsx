f6c41fa6e05b65612b076e30d241caa5786e17c5
tandai katsuaki <>
2023-04-02 21:19:57 +0900

    fix: api url to axios

--- Frontend/src/pages/register/Register.jsx
+++ Frontend/src/pages/register/Register.jsx
@@ -3,15 +3,15 @@ import "./Register.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const client = axios.create({
-  baseURL: process.env.BACKEND_ACCESS,
-});
-
 export default function Register() {
   const username = useRef();
   const email = useRef(); //値を監視できる ref={}属性をつけて使う
   const password = useRef();
   const passwordConfirmation = useRef();
+
+  const client = axios.create({
+    baseURL: process.env.BACKEND_ACCESS,
+  });
 
   const navigate = useNavigate(); //App.jsのNavigateコンポーネントと同じ役割（react-router-dom)
 
@@ -31,7 +31,7 @@ export default function Register() {
         };
 
         //registerAPIを叩く
-        await client.post("/auth/register", user);
+        await client("/auth/register", user);
         //home画面に移動する
         navigate("/login");
       } catch (err) {
